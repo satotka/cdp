85b59b47549f23b3d22312172644f672dac06979
Takaaki SATOH <takaaki.satoh@gmail.com>
2016-11-10 23:26:10 +0000

    update index.html. add angular

---
+++ js/main.js
@@ -0,0 +1,34 @@
+var app = angular.module('cdpApp', []);
+
+app.controller('CdpController', ['$http', function($http) {
+  var cdp = this;
+
+  cdp.jobs = [['now loading...']];
+
+  $http({method: 'GET', url: "./tasks.csv"})
+  .then(function(response) {
+    console.log(response.status);
+    cdp.tasks = parseSV(response.data);
+  }, function(response) {
+    console.log(response.status);
+  });
+  $http({method: 'GET', url: "./jobs.csv"})
+  .then(function(response) {
+    console.log(response.status);
+    cdp.jobs = parseSV(response.data);
+  }, function(response) {
+    console.log(response.status);
+  });
+}]);
+
+
+function parseSV(str, delimiter){
+  if(!delimiter) delimiter = ",";
+  return str.split('\n').reduce(function(table,row){
+    if(!table) return;
+    table.push(
+      row.split(delimiter).map(function(d){ return d.trim();}) //余白削除
+    );
+    return table;
+  }, []);
+}
