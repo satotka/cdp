62474eac37a122c11ec27e6e10bddfde06e83fda
Takaaki SATOH <takaaki.satoh@gmail.com>
2016-11-18 10:29:49 +0000

    update index.html.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,34 @@
 var app = angular.module('cdpApp', []);
 
 app.controller('CdpController', ['$http', function($http) {
+  console.time("cdp1");
   var cdp = this;
+  var storage = localStorage;
+  cdp.jobs = JSON.parse(storage.getItem('jobs'));
+  cdp.tasks = JSON.parse(storage.getItem('tasks'));
 
-  cdp.jobs = [['now loading...']];
-  cdp.roles = [['now loading...']];
 
-  $http({method: 'GET', url: "./tasks.csv"})
-  .then(function(response) {
-    console.log(response.status);
-    cdp.tasks = parseSV(response.data);
-  }, function(response) {
-    console.log(response.status);
-  });
-  $http({method: 'GET', url: "./jobs.csv"})
-  .then(function(response) {
-    console.log(response.status);
-    cdp.jobs = parseSV(response.data);
-  }, function(response) {
-    console.log(response.status);
-  });
+  if (!cdp.tasks) {
+    $http({method: 'GET', url: "./tasks.csv"})
+    .then(function(response) {
+      console.log('tasks:' & response.status);
+      cdp.tasks = parseSV(response.data);
+      storage.setItem('tasks',JSON.stringify(cdp.tasks));
+    }, function(response) {
+      console.log(response.status);
+    });
+  }
+  if(!cdp.jobs) {
+    $http({method: 'GET', url: "./jobs.csv"})
+    .then(function(response) {
+      console.log('jobs:' & response.status);
+      cdp.jobs = parseSV(response.data);
+      storage.setItem('jobs',JSON.stringify(cdp.jobs));
+    }, function(response) {
+      console.log(response.status);
+    });
+  }
+  console.timeEnd("cdp1");
 }]);
 
 
