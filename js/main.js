627bbeb2b6873a328faf41a15bf105627fea09b5
Takaaki SATOH <takaaki.satoh@gmail.com>
2016-11-19 09:39:31 +0000

    add sample image for task.

--- js/main.js
+++ js/main.js
@@ -10,6 +10,9 @@ app.controller('CdpController', ['$http', function($http) {
   console.log("data:");
   console.log(cdp.data);
 
+  // test
+  makeObject(cdp.data.tasks);
+
   cdp.addRole = function () {
     cdp.data.roles.push({code: cdp.newRole.code, name: cdp.newRole.name, tasks: []});
     storage.setItem('data', JSON.stringify(cdp.data));
@@ -29,7 +32,8 @@ app.controller('CdpController', ['$http', function($http) {
     return str.split('\n').reduce(function (table, row) {
       if(!table) return;
       table.push(
-        row.split(delimiter).map(function (d) {return d.trim();}) //余白削除
+        //余白削除, " を除去
+        row.split(delimiter).map(function (d) {return d.trim().replace(/\"/g ,'');})
       );
       return table;
     }, []);
@@ -70,3 +74,28 @@ app.controller('CdpController', ['$http', function($http) {
   }
   console.timeEnd("cdp1");
 }]);
+
+
+function makeObject(array) {
+  var header = array.shift();
+  console.log(header);
+  console.log(array.length);
+  var task = [
+    {code: "ST", name: "（計画・実行）戦略"},
+    {code: "PL", name: "（計画・実行）企画"},
+    {code: "DV", name: "（計画・実行）開発"},
+    {code: "US", name: "（計画・実行）利活用"},
+    {code: "EV", name: "（計画・実行）評価・改善"},
+    {code: "MC", name: "管理・統制"},
+    {code: "CM", name: "推進・支援"},
+    {code: "SP", name: "その他業務"}
+  ];
+  array.forEach(function(row) {
+    task = task.map(function(taskarea) {
+      taskarea.cat1 = [];
+      return taskarea;
+    });
+  });
+   
+  return;
+}
