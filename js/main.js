6a433ff0f81aad1945175cdfb70060b90b341fe7
Takaaki SATOH <takaaki.satoh@gmail.com>
2016-11-19 00:48:29 +0000

    refactor parseSV. add clear strage

--- js/main.js
+++ js/main.js
@@ -8,6 +8,20 @@ app.controller('CdpController', ['$http', function($http) {
   cdp.data = tmp ? JSON.parse(tmp) : {};
   console.log("data:");
   console.log(cdp.data);
+  
+  cdp.parseSV = function (str, delimiter){
+    if(!delimiter) delimiter = ",";
+    return str.split('\n').reduce(function (table, row) {
+      if(!table) return;
+      table.push(
+        row.split(delimiter).map(function (d) {return d.trim();}) //余白削除
+      );
+      return table;
+    }, []);
+  };
+  cdp.clearStrage = function () {
+    localStorage.clear();
+  };
 
   if (!cdp.data.tasks) {
     $http({
@@ -16,7 +30,7 @@ app.controller('CdpController', ['$http', function($http) {
     }).success(function(data, status, headers, config) {
       console.time("tasks");
       console.log("tasks status:", status);
-      cdp.data.tasks = parseSV(data);
+      cdp.data.tasks = cdp.parseSV(data);
       storage.setItem('data', JSON.stringify(cdp.data));
       console.timeEnd("tasks");
     }).error(function(data, status, headers, config) {
@@ -31,7 +45,7 @@ app.controller('CdpController', ['$http', function($http) {
     }).success(function(data, status, headers, config) {
       console.time("jobs");
       console.log("jobs status:", status);
-      cdp.data.jobs = parseSV(data);
+      cdp.data.jobs = cdp.parseSV(data);
       storage.setItem('data', JSON.stringify(cdp.data));
       console.timeEnd("jobs");
     }).error(function(data, status, headers, config) {
@@ -43,13 +57,3 @@ app.controller('CdpController', ['$http', function($http) {
 }]);
 
 
-function parseSV(str, delimiter){
-  if(!delimiter) delimiter = ",";
-  return str.split('\n').reduce(function(table,row){
-    if(!table) return;
-    table.push(
-      row.split(delimiter).map(function(d){ return d.trim();}) //余白削除
-    );
-    return table;
-  }, []);
-}
